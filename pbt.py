ef90c9c3120f32ee79fce5644049830a1880729a
dydones <diegoarbulu7@gmail.com>
2022-05-04 01:01:16 +0200

    Writing of creward and hyperparameters into JSON file

--- pbt.py
+++ pbt.py
@@ -6,6 +6,7 @@ import gym
 import hydra
 import matplotlib.pyplot as plt
 import torch
+import json
 from gym.wrappers import TimeLimit
 from omegaconf import OmegaConf
 from salina import (Agent, Workspace, get_arguments, get_class,
@@ -118,7 +119,8 @@ class PBTAgent:
 
 def create_population(cfg):
     # Create the required number of agents
-    population = []
+    population_r = [] 	#Population changing based on ranking
+    population_o = [] 	#Population staying in the same order
     workspaces = {} # A dictionary of the workspace of each agent
 
     for i in range(cfg.algorithm.population_size):
@@ -129,10 +131,11 @@ def create_population(cfg):
         workspace = Workspace()
 
         # 3) Store them
-        population.append(pbt_agent)
+        population_r.append(pbt_agent)
+        population_o.append(pbt_agent)
         workspaces[pbt_agent] = workspace
         
-    return population, workspaces
+    return population_r, population_o, workspaces
 
 def create_optimizer(cfg: OmegaConf, action_agent: Agent, critic_agent: Agent):
     optimizer_args = get_arguments(cfg.optimizer)
@@ -140,7 +143,7 @@ def create_optimizer(cfg: OmegaConf, action_agent: Agent, critic_agent: Agent):
     optimizer = get_class(cfg.optimizer)(parameters, **optimizer_args)
     return optimizer
 
-def train(cfg, population: List[PBTAgent], workspaces: Dict[Agent, Workspace], logger: TFLogger):
+def train(cfg, population_r: List[PBTAgent], population_o: List[PBTAgent], workspaces: Dict[Agent, Workspace], data: Dict[int,Agent], logger: TFLogger):
     # 1) Prepare the logger and initialize the variables
     epoch_logger = CrewardsLogger()
     total_timesteps = 0
@@ -148,7 +151,7 @@ def train(cfg, population: List[PBTAgent], workspaces: Dict[Agent, Workspace], l
     # 2) Train the agents
     for epoch in range(cfg.algorithm.max_epochs):
         print("Epoch: {}".format(epoch))
-        for agent in population: # TODO: Not train the first one, print his model to see if it changes
+        for agent in population_o: # TODO: Not train the first one, print his model to see if it changes
             consumed_budget = 0
             workspace = workspaces[agent]
             while consumed_budget < cfg.algorithm.train_budget:
@@ -171,7 +174,7 @@ def train(cfg, population: List[PBTAgent], workspaces: Dict[Agent, Workspace], l
             
         total_timesteps += consumed_budget
 
-        all_agents_total_timesteps = total_timesteps * len(population)
+        all_agents_total_timesteps = total_timesteps * len(population_r)
 
         # They have all finished executing
         print('Finished epoch {}'.format(epoch))
@@ -180,23 +183,37 @@ def train(cfg, population: List[PBTAgent], workspaces: Dict[Agent, Workspace], l
         ### PBT EXPLOIT/EXPLORE ###
         ###########################
 
-        crewards = {agent: agent.get_creward() for agent in population}
+        crewards = {agent: agent.get_creward() for agent in population_o}
 
         mean_crewards = torch.mean(torch.stack(list(crewards.values())))
 
         logger.add_log("reward", mean_crewards, all_agents_total_timesteps) # TODO: Print the total num of timesteps
 
         # We sort the agents by their performance
-        sort_performance(population, crewards)
+        sort_performance(population_r, crewards)
 
         print('Cumulated rewards at epoch {}: {}'.format(epoch, crewards.values()))
 
         epoch_logger.log_epoch(all_agents_total_timesteps, torch.tensor(list(crewards.values())))
-        plot_hyperparams([a.action_agent.a2c_agent for a in population])
+        #plot_hyperparams([a.action_agent.a2c_agent for a in population_o])
 
-        for bad_agent in population[-1 * int(cfg.algorithm.pbt_portion * len(population)) : ]:
+        data[all_agents_total_timesteps] = {}
+        for (i,a) in enumerate(population_o):
+	        hyperparams = {}
+	        for hyperparam in a.action_agent.a2c_agent.params.keys():
+	                hyperparams[hyperparam] = 0.0
+	        for hyperparam in hyperparams.keys():
+	                hyperparams[hyperparam] = float(a.action_agent.a2c_agent.params[hyperparam])
+	                #print(hyperparams)
+	                
+	        data[all_agents_total_timesteps][i+1] = {
+	        "creward": float(a.get_creward()),
+	        "hyperparams" : hyperparams
+	        }
+
+        for bad_agent in population_r[-1 * int(cfg.algorithm.pbt_portion * len(population_r)) : ]:
             # Select randomly one agent to replace the current one
-            agent_to_copy = select_pbt(cfg.algorithm.pbt_portion, population)
+            agent_to_copy = select_pbt(cfg.algorithm.pbt_portion, population_r)
             print('Copying agent with creward = {} into agent with creward {}'.format(crewards[agent_to_copy], crewards[bad_agent]))
             bad_agent.copy(agent_to_copy, cfg)
             bad_agent.mutate_hyperparameters()
@@ -204,7 +221,10 @@ def train(cfg, population: List[PBTAgent], workspaces: Dict[Agent, Workspace], l
         #for _, workspace in workspaces.items():
         #    workspace.zero_grad()
     
-    epoch_logger.show()
+            #print(data)
+            with open('/home/diego/projet-recherche-lu3in013-2022/data.json','w') as outfile:
+    	        json.dump(data, outfile)
+            #epoch_logger.show()
 
 
 @hydra.main(config_path=".", config_name="pbt.yaml")
@@ -212,9 +232,10 @@ def main(cfg):
     # First, build the  logger
     logger = Logger(cfg)
     torch.manual_seed(cfg.algorithm.stochasticity_seed)
-    population, workspaces = create_population(cfg)
+    population_r, population_o, workspaces = create_population(cfg)
+    data = {}
     #population[0].load('/home/acmc/repos/projet-recherche-lu3in013-2022/saved_agents/agent_28.pickle', cfg)
-    train(cfg, population, workspaces, logger=logger)
+    train(cfg, population_r, population_o, workspaces, data, logger=logger)
 
 if __name__ == '__main__':
     main()
