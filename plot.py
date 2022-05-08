c6c62d84476ac4c9e8e84a25832eabfd2860dbbe
dydones <62013622+dydones@users.noreply.github.com>
2022-05-08 12:22:59 +0200

    Update plot.py
    added combined plotting

--- plot.py
+++ plot.py
@@ -31,7 +31,7 @@ def plot_hyperparams(agents_list):
             hyperparams[hyperparam] = torch.cat((hyperparams[hyperparam], torch.tensor([agent.params[hyperparam]])))
     plt.boxplot(hyperparams.values())
     plt.xticks(range(1, len(hyperparams) + 1), hyperparams.keys())
-    plt.savefig('/home/acmc/repos/projet-recherche-lu3in013-2022/hyperparams.png')
+    plt.savefig('/home/diego/projet-recherche-lu3in013-2022/hyperparams.png')
 
 class CustomLogger:
     def __init__(self, config=None) -> None:
@@ -42,7 +42,7 @@ class CustomLogger:
             self.population_size = config.algorithm.population_size
             self.output_location = os.path.join(self.dir_location, 'raw_data')
             self.output_filename = os.path.join(self.output_location, 'output_{env}_{size}_{time}.json'.format(env=self.environment_name, size=self.population_size, time=datetime.now().strftime("%d-%m_%H:%M:%S")))
-
+                        
     def log_epoch(self, timestep, crewards, agents):
         self.data[timestep] = {}
         for (i, a) in enumerate(agents):
@@ -140,67 +140,197 @@ class CustomLogger:
         plt.plot(timesteps, mean_values, color=line_color, linewidth=2)
         self.ax.set(xlabel='timestep', ylabel=hyperparam, title='Moyenne et écart type de ' + hyperparam)
 
+class CombinedGraphMaker :
+        def __init__(self):
+                self.data = {} #This is a dict of data from different graphs. It's formatted as follows {size : data}
+                
+        def load(self,file):
+                with open(file,'r') as myfile:
+                        json_data = {}
+                        json_data = json.load(myfile)
+                        size = len(list(json_data.items())[0][1]) #You get the size of the population from the first item of the json file
+                        self.data[size] = json_data
+                myfile.close()
+        
+        def start_plot(self):
+                plt.close()
+                self.fig, self.ax = plt.subplots()
+                self.fig.set_size_inches(10,6)
+                self.ax.get_xaxis().set_major_locator(ticker.AutoLocator())
+                self.ax.grid()
+                self.ax.ticklabel_format(axis='x', style='sci', scilimits=(0,0))
+    
+        def end_plot(self, file):
+                plt.savefig(file)
+                
+        def get_all_rewards(self,data):
+                all_rewards = torch.tensor([])
+                for timestep in data.keys():
+                        timestep_rewards = torch.tensor([agent['reward']  for agent in data[timestep].values()])
+                        all_rewards=torch.cat((all_rewards, timestep_rewards.unsqueeze(0)))
+                return all_rewards   
+                
+        def get_all_hyperparam_values(self, data, hyperparam):
+                all_values = torch.tensor([])
+                for timestep in data.keys():
+                        timestep_values = torch.tensor([agent['hyperparameters'][hyperparam] for agent in data[timestep].values()])
+                        all_values=torch.cat((all_values, timestep_values.unsqueeze(0)))
+                return all_values
+                
+        def plot_rewards_mean_and_individuals(self, line_color='blue'):
+                for i in self.data.keys():
+                        data_i = self.data[i]
+                        timesteps = data_i.keys()
+                        all_rewards = self.get_all_rewards(data_i)
+                        self.ax.set_ylim([min(0, all_rewards.min().item()), max(0, all_rewards.max().item())])
+                        mean_rewards = all_rewards.mean(1)
+                        agents = range(all_rewards.size(1))
+                        timesteps = [int(t) / len(agents) for t in data_i.keys()]  # Adjust the timesteps, to show the number of timesteps that each agent has done
+                        for a in agents:
+                                plt.plot(timesteps, all_rewards.select(1, a), color='grey', alpha=0.3, linewidth=1)
+                                #plt.scatter(timesteps, mean_rewards, color=line_color)
+                        plt.plot(timesteps, mean_rewards, color=line_color, linewidth=2, label=i)
+                plt.legend()
+                self.ax.set(xlabel='timestep',ylabel='reward', title='Combinaison des évolutions individuelles de la récompense')
+
+        def plot_rewards_mean_and_std(self, line_color='blue'):
+                for i in self.data.keys():
+                        data_i = self.data[i]
+                        all_rewards = self.get_all_rewards(data_i)
+                        self.ax.set_ylim([min(0, all_rewards.min().item()), max(0, all_rewards.max().item())])
+                        mean_rewards = all_rewards.mean(1)
+                        std_rewards = all_rewards.std(1)
+                        agents = range(all_rewards.size(1))
+                        timesteps = [int(t) / len(agents) for t in data_i.keys()]  # Adjust the timesteps, to show the number of timesteps that each agent has done
+                        plt.fill_between(timesteps, mean_rewards - std_rewards, mean_rewards + std_rewards, color=line_color, alpha=0.5)
+                        #plt.scatter(timesteps, mean_rewards, color=line_color)
+                        plt.plot(timesteps, mean_rewards, color=line_color, linewidth=2,label=i)
+                plt.legend()
+                self.ax.set(xlabel='timestep', ylabel='reward', title='Moyenne et écart type combinés des récompenses')        
+
+        def plot_hyperparam_individuals(self, hyperparam, line_color='blue'):
+                for i in self.data.keys():
+                        data_i = self.data[i]
+                        all_values = self.get_all_hyperparam_values(data_i,hyperparam)
+                        self.ax.set_ylim([0, all_values.max().item()])
+                        agents = range(all_values.size(1))
+                        timesteps = [int(t) / len(agents) for t in data_i.keys()]  # Adjust the timesteps, to show the number of timesteps that each agent has done
+                        mean_values = all_values.mean(1)
+                        for a in agents:
+                                plt.plot(timesteps, all_values.select(1, a), color='grey', alpha=0.3, linewidth=1)
+                                #plt.scatter(timesteps, mean_values, color=line_color)
+                        plt.plot(timesteps, mean_values, color=line_color, linewidth=2,label=i)
+                plt.legend()
+                self.ax.set(xlabel='timestep', ylabel=hyperparam, title='Combinaison des évolutions individuelles de ' + hyperparam)
+
+        def plot_hyperparam_mean_and_std(self, hyperparam, line_color='blue'):
+                for i in self.data.keys():
+                        data_i = self.data[i]
+                        all_values = self.get_all_hyperparam_values(data_i,hyperparam)
+                        self.ax.set_ylim([0, all_values.max().item()])
+                        mean_values = all_values.mean(1)
+                        std_values = all_values.std(1)
+                        agents = range(all_values.size(1))
+                        timesteps = [int(t) / len(agents) for t in data_i.keys()]  # Adjust the timesteps, to show the number of timesteps that each agent has done
+                        plt.fill_between(timesteps, mean_values - std_values, mean_values + std_values, color=line_color, alpha=0.5)
+                        #plt.scatter(timesteps, mean_values, color=line_color)
+                        plt.plot(timesteps, mean_values, color=line_color, linewidth=2,label=i)
+                plt.legend()
+                self.ax.set(xlabel='timestep', ylabel=hyperparam, title='Moyenne et écart type combinés de ' + hyperparam)
 
 if __name__ == "__main__":
     filename = sys.argv[1]
+    filelist = []
+    for i in range(5):
+        filelist.append(sys.argv[i+1])
+    
+    #logger = CustomLogger()
+    #logger.open('./raw_data/{}'.format(filename))
 
-    logger = CustomLogger()
-    logger.open(filename)
-
-    output_dir = 'graphs/{}/'.format(os.path.basename(filename))
+    output_dir = './graphs/{}/'.format(os.path.basename(filename))
     try:
         os.mkdir(output_dir) # Create the directory if it does not exist
     except FileExistsError:
         pass
-
+        
+    graphmaker = CombinedGraphMaker()
+    for i in range(5):
+        print(filelist[i])
+        graphmaker.load('./raw_data/{}'.format(filelist[i]))
+        
+    graphmaker.start_plot()
+    graphmaker.plot_rewards_mean_and_individuals()
+    graphmaker.end_plot(output_dir + 'combined_reward_individuals.png')
+    graphmaker.start_plot()
+    graphmaker.plot_rewards_mean_and_std()
+    graphmaker.end_plot(output_dir + 'reward_std.png')
+    graphmaker.start_plot()
+    graphmaker.plot_hyperparam_individuals('a2c_coef', line_color='green')
+    graphmaker.end_plot(output_dir + 'a2c_coef_inds.png')
+    graphmaker.start_plot()
+    graphmaker.plot_hyperparam_mean_and_std('a2c_coef', line_color='green')
+    graphmaker.end_plot(output_dir + 'a2c_coef_mean_and_std.png')
+    graphmaker.start_plot()
+    graphmaker.plot_hyperparam_individuals('critic_coef', line_color='red')
+    graphmaker.end_plot(output_dir + 'critic_coef_inds.png')
+    graphmaker.start_plot()
+    graphmaker.plot_hyperparam_mean_and_std('critic_coef', line_color='red')
+    graphmaker.end_plot(output_dir + 'critic_coef_mean_and_std.png')
+    graphmaker.start_plot()
+    graphmaker.plot_hyperparam_individuals('entropy_coef', line_color='orange')
+    graphmaker.end_plot(output_dir + 'entropy_coef_inds.png')
+    graphmaker.start_plot()
+    graphmaker.plot_hyperparam_mean_and_std('entropy_coef', line_color='orange')
+    graphmaker.end_plot(output_dir + 'entropy_coef_mean_and_std.png')
+    
     # Graphs in PNG
-    logger.start_plot()
-    logger.plot_rewards_mean_and_individuals()
-    logger.end_plot(output_dir + 'reward_individuals.png')
-    logger.start_plot()
-    logger.plot_rewards_mean_and_std()
-    logger.end_plot(output_dir + 'reward_std.png')
-    logger.start_plot()
-    logger.plot_hyperparam_individuals('a2c_coef', line_color='green')
-    logger.end_plot(output_dir + 'a2c_coef_inds.png')
-    logger.start_plot()
-    logger.plot_hyperparam_mean_and_std('a2c_coef', line_color='green')
-    logger.end_plot(output_dir + 'a2c_coef_mean_and_std.png')
-    logger.start_plot()
-    logger.plot_hyperparam_individuals('critic_coef', line_color='red')
-    logger.end_plot(output_dir + 'critic_coef_inds.png')
-    logger.start_plot()
-    logger.plot_hyperparam_mean_and_std('critic_coef', line_color='red')
-    logger.end_plot(output_dir + 'critic_coef_mean_and_std.png')
-    logger.start_plot()
-    logger.plot_hyperparam_individuals('entropy_coef', line_color='orange')
-    logger.end_plot(output_dir + 'entropy_coef_inds.png')
-    logger.start_plot()
-    logger.plot_hyperparam_mean_and_std('entropy_coef', line_color='orange')
-    logger.end_plot(output_dir + 'entropy_coef_mean_and_std.png')
+    #logger.start_plot()
+    #logger.plot_rewards_mean_and_individuals()
+    #logger.end_plot(output_dir + 'reward_individuals.png')
+    #logger.start_plot()
+    #logger.plot_rewards_mean_and_std()
+    #logger.end_plot(output_dir + 'reward_std.png')
+    #logger.start_plot()
+    #logger.plot_hyperparam_individuals('a2c_coef', line_color='green')
+    #logger.end_plot(output_dir + 'a2c_coef_inds.png')
+    #logger.start_plot()
+    #logger.plot_hyperparam_mean_and_std('a2c_coef', line_color='green')
+    #logger.end_plot(output_dir + 'a2c_coef_mean_and_std.png')
+    #logger.start_plot()
+    #logger.plot_hyperparam_individuals('critic_coef', line_color='red')
+    #logger.end_plot(output_dir + 'critic_coef_inds.png')
+    #logger.start_plot()
+    #logger.plot_hyperparam_mean_and_std('critic_coef', line_color='red')
+    #logger.end_plot(output_dir + 'critic_coef_mean_and_std.png')
+    #logger.start_plot()
+    #logger.plot_hyperparam_individuals('entropy_coef', line_color='orange')
+    #logger.end_plot(output_dir + 'entropy_coef_inds.png')
+    #logger.start_plot()
+    #logger.plot_hyperparam_mean_and_std('entropy_coef', line_color='orange')
+    #logger.end_plot(output_dir + 'entropy_coef_mean_and_std.png')
 
     # Graphs in PDF, for LaTeX
-    logger.start_plot()
-    logger.plot_rewards_mean_and_individuals()
-    logger.end_plot(output_dir + 'reward_individuals.pdf')
-    logger.start_plot()
-    logger.plot_rewards_mean_and_std()
-    logger.end_plot(output_dir + 'reward_std.pdf')
-    logger.start_plot()
-    logger.plot_hyperparam_individuals('a2c_coef', line_color='green')
-    logger.end_plot(output_dir + 'a2c_coef_inds.pdf')
-    logger.start_plot()
-    logger.plot_hyperparam_mean_and_std('a2c_coef', line_color='green')
-    logger.end_plot(output_dir + 'a2c_coef_mean_and_std.pdf')
-    logger.start_plot()
-    logger.plot_hyperparam_individuals('critic_coef', line_color='red')
-    logger.end_plot(output_dir + 'critic_coef_inds.pdf')
-    logger.start_plot()
-    logger.plot_hyperparam_mean_and_std('critic_coef', line_color='red')
-    logger.end_plot(output_dir + 'critic_coef_mean_and_std.pdf')
-    logger.start_plot()
-    logger.plot_hyperparam_individuals('entropy_coef', line_color='orange')
-    logger.end_plot(output_dir + 'entropy_coef_inds.pdf')
-    logger.start_plot()
-    logger.plot_hyperparam_mean_and_std('entropy_coef', line_color='orange')
-    logger.end_plot(output_dir + 'entropy_coef_mean_and_std.pdf')
+    #logger.start_plot()
+    #logger.plot_rewards_mean_and_individuals()
+    #logger.end_plot(output_dir + 'reward_individuals.pdf')
+    #logger.start_plot()
+    #logger.plot_rewards_mean_and_std()
+    #logger.end_plot(output_dir + 'reward_std.pdf')
+    #logger.start_plot()
+    #logger.plot_hyperparam_individuals('a2c_coef', line_color='green')
+    #logger.end_plot(output_dir + 'a2c_coef_inds.pdf')
+    #logger.start_plot()
+    #logger.plot_hyperparam_mean_and_std('a2c_coef', line_color='green')
+    #logger.end_plot(output_dir + 'a2c_coef_mean_and_std.pdf')
+    #logger.start_plot()
+    #logger.plot_hyperparam_individuals('critic_coef', line_color='red')
+    #logger.end_plot(output_dir + 'critic_coef_inds.pdf')
+    #logger.start_plot()
+    #logger.plot_hyperparam_mean_and_std('critic_coef', line_color='red')
+    #logger.end_plot(output_dir + 'critic_coef_mean_and_std.pdf')
+    #logger.start_plot()
+    #logger.plot_hyperparam_individuals('entropy_coef', line_color='orange')
+    #logger.end_plot(output_dir + 'entropy_coef_inds.pdf')
+    #logger.start_plot()
+    #logger.plot_hyperparam_mean_and_std('entropy_coef', line_color='orange')
+    #logger.end_plot(output_dir + 'entropy_coef_mean_and_std.pdf')
